0ca18537c094af4a8a93decd8c0469b241690527
Faridun Rahimov <farid.istar05@gmail.com>
2026-08-23 16:34:38 +0500

    refactor: remove unused prisma.config.ts and update schema.prisma to use environment variable for DATABASE_URL

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-
-export default {
-  schema: 'prisma/schema.prisma',
-  datasource: {
-    url: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/mobile_shop_db?schema=public',
-  },
-};
