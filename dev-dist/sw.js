5a5e98b1758e0bed957ee0921be8361f5a1854e8
Faridun Rahimov <farid.istar05@gmail.com>
2026-08-30 16:37:12 +0500

    feat: add service worker implementation using Workbox for offline caching and asset management

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-3dc3dd85'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jt38tuqtvoo"
+    "revision": "0.bih4q09gkuo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
